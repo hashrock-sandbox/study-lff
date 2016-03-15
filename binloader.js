54ee49dba45deeaccd09c3756af3ad5d98ae1b82
hashrock <hashedrock@gmail.com>
2016-03-15 16:44:02 +0900

    write wrong buffer

---
+++ binloader.js
@@ -0,0 +1,25 @@
+var fs = require("fs");
+
+var buf = fs.readFileSync("kst32b.bin");
+
+var offset = 0;
+while(offset < buf.length){
+    var characterBuf = new Buffer(4);
+    buf.copy(characterBuf, 0, offset, 4);
+    console.log(characterBuf);
+    var characterStr = characterBuf.toString("utf-8");
+    console.log("@"+ characterStr);
+    offset += 4;
+
+    var strokeNum = buf.readUInt16LE(offset);
+    //console.log(":"+ strokeNum);
+    offset += 2;
+    for(var i = 0; i < strokeNum; i++){
+        var x0 = buf.readUInt8(offset + 0);
+        var y0 = buf.readUInt8(offset + 1);
+        var x1 = buf.readUInt8(offset + 2);
+        var y1 = buf.readUInt8(offset + 3);
+        //console.log(x0 + ", " + y0 + ", " + x1 + ", " + y1);
+        offset += 4;
+    }
+}
--- loader.js
+++ loader.js
@@ -8,3 +8,45 @@ var data = p(data.split("\n"));
 fs.writeFileSync( "kst32b.json",JSON.stringify(data), "utf-8");
 console.log(data.length + "char done.");
 
+var buf = new Buffer(1000 * 1000);
+var offset = 0;
+
+var codemax = 0;
+
+for(var i = 0; i < data.length; i++){
+    var font = data[i];
+    //文字の書き込み
+    buf.write(font[0], offset, 4);
+    console.log("@"+ font[0]);
+    console.log("@@"+ font[0].codePointAt(0));
+    console.log("@@@"+String.fromCodePoint(font[0].codePointAt(0)));
+    var cp = font[0].codePointAt(0);
+    if(codemax < cp){
+        codemax = cp;
+    }
+    offset += 4;
+
+    //ストロークの数
+    buf.writeUInt16LE(font[1].length, offset);
+    console.log(":"+ font[1].length);
+    offset += 2;
+    
+    //ストローク（4つ一組）
+    for(var x = 0; x < 4; x++){
+        var f = parseFloat(font[1][x]);
+        var fi = Math.round(256 * f / 10);
+        buf.writeUInt8(fi, offset);
+        offset += 1;
+        console.log(","+ fi);
+    }
+}
+
+var outbuf = new Buffer(offset);
+buf.copy(outbuf, 0, 0, offset);
+
+console.log("MAX: " + codemax);
+
+console.log(offset / 1000 + "KB");
+
+fs.writeFileSync("kst32b.bin", outbuf);
+
